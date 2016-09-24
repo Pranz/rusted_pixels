5c5466b295d0667351658cb4603f4708c19bbb02
Jesper Fridefors <jesper.fridefors@gmail.com>
2016-09-24 20:44:27 +0200

    refactor util

--- src/util.rs
+++ src/util.rs
@@ -2,42 +2,49 @@ use sdl2::pixels::Color;
 
 pub fn parse_color(string: &str) -> Option<Color> {
     if string.starts_with("rgb ") {
-        
-        let filtered = string[4..]
-            .chars()
-            .filter(|c| *c != ' ')
-            .collect::<String>();
-        let splitted = filtered
-            .split(',')
-            .collect::<Vec<&str>>();
-        
-        if splitted.len() == 3 {
-            if let (Ok(r), Ok(g), Ok(b)) = (splitted[0].parse::<u8>(),
-                                            splitted[1].parse::<u8>(),
-                                            splitted[2].parse::<u8>())
-            { return Some(Color::RGB(r,g,b)); }
-        }
+        parse_rgb(string[4..])
     } else if string.starts_with("rgba ") {
-        
-        let filtered = string[5..]
-            .chars()
-            .filter(|c| *c != ' ')
-            .collect::<String>();
-        let splitted = filtered
-            .split(',')
-            .collect::<Vec<&str>>();
-        
-        if splitted.len() == 4 {
-            if let (Ok(r), Ok(g), Ok(b), Ok(a))
-                = (splitted[0].parse::<u8>(),
-                   splitted[1].parse::<u8>(),
-                   splitted[2].parse::<u8>(),
-                   splitted[3].parse::<u8>())
-            { return Some(Color::RGBA(r,g,b,a)); }
-        }
+        parse_rgba(string[5..])
     } else if string.starts_with('#') {
         //TODO: hexadecimal conversion
-        return None;
+        None
+    } else {
+        None
     }
-    return None;
+}
+
+pub fn parse_rgb(string: &str) {
+    string
+        .chars()
+        .filter(|c| *c != ' ')
+        .collect::<String>();
+    let splitted = filtered
+        .split(',')
+        .collect::<Vec<&str>>();
+    
+    if splitted.len() == 3 {
+        if let (Ok(r), Ok(g), Ok(b)) = (splitted[0].parse::<u8>(),
+                                        splitted[1].parse::<u8>(),
+                                        splitted[2].parse::<u8>())
+        { return Some(Color::RGB(r,g,b)); }
+    }
+}
+
+pub fn parse_rgba(string: &str) {
+    string
+        .chars()
+        .filter(|c| *c != ' ')
+        .collect::<String>();
+    let splitted = filtered
+        .split(',')
+        .collect::<Vec<&str>>();
+    
+    if splitted.len() == 4 {
+        if let (Ok(r), Ok(g), Ok(b), Ok(a))
+            = (splitted[0].parse::<u8>(),
+               splitted[1].parse::<u8>(),
+               splitted[2].parse::<u8>(),
+               splitted[3].parse::<u8>())
+        { return Some(Color::RGBA(r,g,b,a)); }
+    }
 }
