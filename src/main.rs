a8f158d98dcb0b69f3043f467095aa3a0f329145
Jesper Fridefors <jesper.fridefors@gmail.com>
2016-09-24 21:34:18 +0200

    refactored main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate png;
 
 use sdl2::pixels::Color;
 use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
+use sdl2::keyboard::{Keycode, Mod};
 use sdl2::render::BlendMode;
 use sdl2::mouse::Mouse;
 use sdl2::video::Window as SdlWindow;
@@ -49,61 +49,17 @@ pub fn main() {
                 },
                 Event::MouseButtonDown { mouse_btn: Mouse::Left,
                                          x, y, .. } => {
-                    state.left_mouse_down = true;
-                    for window in &windows {
-                        window
-                            .handle_mouse_down(&mut state, x, y);
-                    }
+                    handle_mouse_left_down(&mut state, &windows, x, y);
                 },
                 Event::MouseMotion { x, y, .. } => {
-                    state.mouse_x = x;
-                    state.mouse_y = y;
-                    if state.left_mouse_down {
-                        for window in &windows {
-                            window
-                                .handle_mouse_down(&mut state, x, y);
-                        }
-                    }
+                    handle_mouse_motion(&mut state, &windows, x, y);
                 },
                 Event::MouseButtonUp { mouse_btn: Mouse::Left, .. } => {
                     state.left_mouse_down = false;
                 },
                 Event::KeyDown { keycode: Some(keycode), keymod, .. } => {
-                    // every command begins with a single key
-                    if state.input.is_empty() {
-                        state.input.push(Input::Char(keycode,keymod));
-                        match execute_command(&mut state, &commands) {
-                            CommandResult::Quit => { break 'main_loop },
-                            _ => {}
-                        }
-                    }
-                    // If escape is pressed, clear input buffer or pop
-                    // input stack
-                    else if keycode == Keycode::Escape {
-                        if !state.input_buffer.is_empty() {
-                            state.input_buffer = String::new();
-                        } else {
-                            state.input.pop();
-                        }
-                    }
-                    else if keycode == Keycode::Return {
-                        let (input_type, arg)
-                            = input::parse_input(&state.input_buffer);
-                        state.input.push(input_type);
-                        if let Some(arg) = arg {
-                            state.args.push(arg);
-                        }
-                        state.input_buffer = String::new();
-                        match execute_command(&mut state, &commands) {
-                            CommandResult::Quit => { break 'main_loop },
-                            _ => {}
-                        }
-                    }
-                    else {
-                        if let Some(chr) = input::keycode_to_char(keycode) {
-                            state.input_buffer.push(chr);
-                            println!("{:?}", state.input_buffer.as_str());
-                        }
+                    if handle_key_down(&mut state, &commands, keycode, keymod) {
+                        break 'main_loop;
                     }
                 },
                 _ => {}
@@ -122,7 +78,67 @@ pub fn main() {
     }
 }
 
-pub fn initialize_windows() -> Vec<Box<Window>> {
+fn handle_mouse_left_down(state: &mut State, windows: &[Box<Window>],
+                          x: i32, y: i32) {
+    state.left_mouse_down = true;
+    for window in windows {
+        window.handle_mouse_down(state, x, y);
+    }
+}
+
+fn handle_mouse_motion(state: &mut State, windows: &[Box<Window>],
+                       x: i32, y: i32) {
+    state.mouse_x = x;
+    state.mouse_y = y;
+    if state.left_mouse_down {
+        for window in windows {
+            window.handle_mouse_down(state, x, y);
+        }
+    }
+}
+
+fn handle_key_down(state: &mut State, commands: &[(Vec<Input>, Command)],
+                   keycode: Keycode, keymod: Mod) -> bool {
+    // every command begins with a single key
+    if state.input.is_empty() {
+        state.input.push(Input::Char(keycode,keymod));
+        match execute_command(state, commands) {
+            CommandResult::Quit => { return true },
+            _ => {}
+        }
+    }
+    // If escape is pressed, clear input buffer or pop
+    // input stack
+    else if keycode == Keycode::Escape {
+        if !state.input_buffer.is_empty() {
+            state.input_buffer = String::new();
+        } else {
+            state.input.pop();
+        }
+    }
+    else if keycode == Keycode::Return {
+        let (input_type, arg)
+            = input::parse_input(&state.input_buffer);
+        state.input.push(input_type);
+        if let Some(arg) = arg {
+            state.args.push(arg);
+        }
+                        state.input_buffer = String::new();
+        match execute_command(state, commands) {
+            CommandResult::Quit => { return true },
+            _ => {}
+        }
+    }
+    else {
+        if let Some(chr) = input::keycode_to_char(keycode) {
+            state.input_buffer.push(chr);
+            println!("{:?}", state.input_buffer.as_str());
+        }
+    }
+    return false;
+}
+
+fn initialize_windows() -> Vec<Box<Window>> {
     let gray = Color::RGB(50, 50, 50);
     let lighter_gray = Color::RGB(100, 100, 100);
     vec![Box::new(DrawingWindow::new(50, 50, 8, lighter_gray, 0)),
@@ -131,7 +147,7 @@ pub fn initialize_windows() -> Vec<Box<Window>> {
          Box::new(PaletteWindow{x: 400,y: 100,palette_id: 0})]
 }
 
-pub fn init_sdl_window(sdl_context: &Sdl) -> SdlWindow {
+fn init_sdl_window(sdl_context: &Sdl) -> SdlWindow {
     let video_subsystem = sdl_context.video().unwrap();
     video_subsystem.window("rusted pixels", 800, 600)
         .resizable()
